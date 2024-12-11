6ad2c4ec12b41184993d9599a26561df624872db
JamesW-SCAS <James.Wise@scas.nhs.uk>
2024-12-11 12:50:37 +0000

    first working 12 lead ID example

--- clinical_nlp.py
+++ clinical_nlp.py
@@ -14,7 +14,7 @@ filtered_cols = [
     ]
 df = pd.read_csv("nlp_input.csv"
 , usecols = filtered_cols
-, nrows=15
+, nrows=100
 )
 # Drop any rows with null data
 df.dropna(
@@ -22,9 +22,9 @@ df.dropna(
  inplace=True)
 
 # TEST ROW FOR 12 LEAD ECG - Remove later
-df = df[3:4] # Row contains a great positive/negative 12 lead example 
+# df = df[3:4] # Row contains a great positive/negative 12 lead example 
 # force in some text to check how Spacy handles various entries
-df.iloc[0,1] = "12 lead, 12lead, twelve lead, twelvelead"
+# df.iloc[0,1] = "12 lead, 12lead, twelve lead, twelvelead"
 
 # Function to extract named entities
 def extract_named_entities(text):
@@ -42,4 +42,13 @@ print(df.head(3))
 # Visualize the entities in displacy
 for index, row in df.iterrows():
     doc = nlp(row['impressionPlan'])
-    displacy.render(doc, style="ent", jupyter=True)
+    # displacy.render(doc, style="ent", jupyter=True)
+    # New loop to ID 12 lead ecg
+    for token in doc:
+        # Check if token is like a number
+        if token.like_num:
+            # Get the next token
+            next_token = doc[token.i + 1]
+            # Check if next token is "lead"
+            if next_token.text == "lead":
+                print("12-lead ECG found:", token.text, next_token.text)
