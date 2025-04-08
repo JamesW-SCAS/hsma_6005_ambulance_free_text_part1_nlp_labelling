e02eb2e3994b1974eaff6341af7ff5f2a11763e6
JamesW-SCAS <James.Wise@scas.nhs.uk>
2025-04-08 16:49:38 +0100

    tidied up code

--- import spacy.py
+++ import spacy.py
@@ -4,23 +4,22 @@ from negspacy.negation import Negex
 from spacy.tokens import Span
 
 nlp = spacy.load("en_core_web_sm")
-# nlp.add_pipe("negex", last=True, config={"ent_types":["ECG_PATTERN"]})
 
 matcher = Matcher(nlp.vocab)
+
 pattern = [{"LOWER": "12"}, {"LOWER": "lead"}, {"LOWER": "ecg"}]
 oxy_pattern = [{"LOWER" : "oxygen"}]
+
 matcher.add("ECG_PATTERN", [pattern])
 matcher.add("O2_PATTERN", [oxy_pattern])
 
 @spacy.Language.component("custom_ents_component")
 def custom_ents_component(doc):
-    # docs = []
     matches = matcher(doc)
     spans = [Span(doc, start, end, label=match_id)
      for match_id, start, end in matches]
     print(spans)
     doc.ents = spans
-    # docs.append(doc)
     return doc
 
 # Try adding components to the pipeline in order, as per Sammi's notebook
