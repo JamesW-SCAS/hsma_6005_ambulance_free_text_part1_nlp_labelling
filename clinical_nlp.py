679b1c1543e667e75d76c80b399cdcf8af044bcf
JamesW-SCAS <James.Wise@scas.nhs.uk>
2025-04-09 15:45:42 +0100

    working negation with label and negation flag columns

--- clinical_nlp.py
+++ clinical_nlp.py
@@ -23,9 +23,9 @@ df = pd.read_csv("Oxygen and 12 Lead Free Text Training Model v0.1 - 20250227.cs
 )
 
 # TEST ROW - Remove later
-# df = df[3:4] # Row contains a great positive/negative 12 lead example 
+df = df[3:4] # Row contains a great positive/negative 12 lead example 
 # force in some text to check how Spacy handles various entries
-# df.iloc[0,1] = "12 lead ecg was done but no oxygen given" #"I'm looking for no oxygen not given, was taken, and no 12 lead here. More oxygen given. And another twelve lead"
+df.iloc[0,1] = "12 lead ecg was done but no oxygen given" #"I'm looking for no oxygen not given, was taken, and no 12 lead here. More oxygen given. And another twelve lead"
 # df.iloc[0,2] = "...and air taken here"
 
 # Initialize the Matcher with the shared vocabulary
@@ -62,7 +62,7 @@ def custom_ents_component(doc):
     matches = matcher(doc)
     spans = [Span(doc, start, end, label=match_id)
      for match_id, start, end in matches]
-    print(spans)
+    # print(spans)
     doc.ents = spans
     return doc
 
@@ -71,6 +71,12 @@ nlp.add_pipe("custom_ents_component", after="ner")
 nlp.add_pipe("negex", last=True, config={
     "ent_types":["12_lead_ecg_label", "oxygen_label"]
     })
+
+# Add columns to receive resulting entity and negation labels, default 0
+df['12_lead_label_found'] = 0
+df['12_lead_label_negated'] = 0
+df['oxygen_label_found'] = 0
+df['oxygen_label_negated'] = 0
 
 # Apply the matcher to each row of the dataframe:
 for index, row in df.iterrows():
@@ -83,3 +89,18 @@ for index, row in df.iterrows():
     print(f"Row {index}:")
     for e in doc.ents:
         print(f"Entity: {e.text}, Label: {e.label_}, Negation: {e._.negex}")
+        # Check if entity label matches a custom entity, and set flag
+        # 12-lead label
+        if e.label_ == '12_lead_ecg_label':
+            df.at[index, "12_lead_label_found"] = 1
+            # Check if the label was negated, and set flag
+            if e._.negex == True:
+                df.at[index, "12_lead_label_negated"] = 1
+        # Oxygen label
+        if e.label_ == 'oxygen_label':
+            df.at[index, "oxygen_label_found"] = 1
+            # Check if the label was negated, and set flag
+            if e._.negex == True:
+                df.at[index, "oxygen_label_negated"] = 1
+
+df
